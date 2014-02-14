3812cc9859ad55459273fa0a71c01f09503258bf
Grady Simon <grady@gradysimon.com>
2014-02-14 17:35:55 -0500

    Added the first API view and corresponding URLconf entry.

---
+++ peppermint_server/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from peppermint_server import views
+
+urlpatterns = patterns('',
+    url(r'^topics/$', views.TopicList.as_view()),
+)
+
+# Enable URL format suffixes - specify requested data format with a suffix
+urlpatterns = format_suffix_patterns(urlpatterns)
--- peppermint_server/views.py
+++ peppermint_server/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from peppermint_server.models import UserProfile, Topic
+from peppermint_server.serializers import UserProfileSerializer, TopicSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class TopicList(APIView):
+    """
+    List all topics, or create a new Topic.
+    """
+    def get(self, request, format=None):
+        topics = Topic.objects.all()
+        serializer = TopicSerializer(topics, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TopicSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
