ef8c73be2db733a660dc6f46a43fe5554392e4e9
Grady Simon <grady@gradysimon.com>
2014-02-14 14:07:42 -0500

    Added the most basic models.

--- peppermint_server/models.py
+++ peppermint_server/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+class Topic(models.Model):
+    author = models.ForeignKey('UserProfile', related_name='topics')
+    text = models.TextField()
+    
