c9810b9a9ecec3c113f83c57627a1ac714b66e2d
Grady Simon <grady@gradysimon.com>
2014-02-27 17:50:58 -0500

    Fixed a validation error in those new models.

--- peppermint_server/models.py
+++ peppermint_server/models.py
@@ -5,6 +5,7 @@ class UserProfile(models.Model):
     last_name = models.CharField(max_length=50)
     intersted_topics = models.ManyToManyField('Topic', through='Interest', related_name='interested_users')
     bored_topics = models.ManyToManyField('Topic', through='Boredom', related_name='bored_users')
+    conversations = models.ManyToManyField('self', through='Conversation', symmetrical=False)
 
 class Topic(models.Model):
     author = models.ForeignKey('UserProfile', related_name='topics')
@@ -21,3 +22,15 @@ class Boredom(models.Model):
     user = models.ForeignKey('UserProfile')
     topic = models.ForeignKey('Topic')
     date = models.DateTimeField(auto_now=True)
+
+class Conversation(models.Model):
+    topic = models.ForeignKey('Topic')
+    initiator = models.ForeignKey('UserProfile', related_name='initiated_conversations')
+    recipient = models.ForeignKey('UserProfile', related_name='received_conversations')
+    start_date = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    conversation = models.ForeignKey('Conversation')
+    sender = models.ForeignKey('UserProfile')
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
